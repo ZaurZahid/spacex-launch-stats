837ca107e463b0a0d45a508958505ac67650afdf
ZaurZahid <aliyevzaurbba@gmail.com>
2020-04-08 01:22:17 +0800

    finalized.

---
+++ public/precache-manifest.10695b719c47a32f5eb0db72a50f774f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06901f97310fe19659c5",
+    "url": "/static/js/main.06901f97.chunk.js"
+  },
+  {
+    "revision": "16c3dbebde294281a833",
+    "url": "/static/js/1.16c3dbeb.chunk.js"
+  },
+  {
+    "revision": "06901f97310fe19659c5",
+    "url": "/static/css/main.80ee5fef.chunk.css"
+  },
+  {
+    "revision": "c9cba6c318a14e162afe4ea61a5c71de",
+    "url": "/index.html"
+  }
+];
